eb09a539d250fe3c8dd324e2bdfdb8247a542893
E2D <engineer2designer@gmail.com>
2025-12-25 11:27:24 +0800

    更新 E2D_git.sh 腳本，移除不必要的拉取命令，簡化子模組初始化流程

--- E2D_git.sh
+++ E2D_git.sh
@@ -1,5 +1,5 @@
 
-git submodule status
+git submodule status (檢查 submodule 狀態)
 
 git -C main/grbl  branch -r  
 git -C main/grbl checkout e2d-master
@@ -7,17 +7,16 @@ git config -f .gitmodules submodule.main/grbl.branch e2d-master
 git submodule sync --recursive
 git submodule update --remote main/grbl
 
-git -C main/networking  branch -r  
+git -C main/networking  branch -r
+git -C main/networking checkout e2d-update  
 git config -f .gitmodules submodule.main/networking.branch e2d-update
 git submodule sync --recursive
 git submodule update --remote main/networking
 
 git -C main/plugins  branch -r    
 git -C main/plugins checkout e2d-update
-
 git config -f .gitmodules submodule.main/plugins.branch e2d-update
 git submodule sync --recursive
 git submodule update --remote main/plugins
 
-git -C main/grbl pull      # 拉 e2d-master 最新
-git add main/grbl .gitmodules
+git submodule update --init --recursive (初始化並更新所有子模組)

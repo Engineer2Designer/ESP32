3aef0f4abaeb72e18803af3d8e8c6a4627540fa7
E2D <engineer2designer@gmail.com>
2026-02-08 13:04:04 +0800

    Refactor code structure for improved readability and maintainability

---
+++ Docs/E2D_git.sh
@@ -0,0 +1,22 @@
+
+git submodule status (檢查 submodule 狀態)
+
+git -C main/grbl  branch -r  
+git -C main/grbl checkout e2d-master
+git config -f .gitmodules submodule.main/grbl.branch e2d-master
+git submodule sync --recursive
+git submodule update --remote main/grbl
+
+git -C main/networking  branch -r
+git -C main/networking checkout e2d-update  
+git config -f .gitmodules submodule.main/networking.branch e2d-update
+git submodule sync --recursive
+git submodule update --remote main/networking
+
+git -C main/plugins  branch -r    
+git -C main/plugins checkout e2d-update
+git config -f .gitmodules submodule.main/plugins.branch e2d-update
+git submodule sync --recursive
+git submodule update --remote main/plugins
+
+git submodule update --init --recursive (初始化並更新所有子模組)

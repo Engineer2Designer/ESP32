723cb8b50fc941de26cf32fca3e869640f9ccb63
E2D <engineer2designer@gmail.com>
2026-02-11 23:38:28 +0800

    更新子模組分支至 DLC32_V1.2 以保持一致性

--- Docs/E2D_git.sh
+++ Docs/E2D_git.sh
@@ -2,20 +2,20 @@
 git submodule status (檢查 submodule 狀態)
 
 git -C main/grbl  branch -r  
-git -C main/grbl checkout e2d-master
-git config -f .gitmodules submodule.main/grbl.branch e2d-master
+git -C main/grbl checkout DLC32_V1.2
+git config -f .gitmodules submodule.main/grbl.branch DLC32_V1.2
 git submodule sync --recursive
 git submodule update --remote main/grbl
 
 git -C main/networking  branch -r
-git -C main/networking checkout e2d-update  
+git -C main/networking checkout DLC32_V1.2 
 git config -f .gitmodules submodule.main/networking.branch e2d-update
 git submodule sync --recursive
 git submodule update --remote main/networking
 
 git -C main/plugins  branch -r    
 git -C main/plugins checkout e2d-update
-git config -f .gitmodules submodule.main/plugins.branch e2d-update
+git config -f .gitmodules submodule.main/plugins.branch DLC32_V1.2
 git submodule sync --recursive
 git submodule update --remote main/plugins
 
